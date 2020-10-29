94290189f9f6c35378f02343c1f8ea0373a6af09
Dustin Herboldshimer <dherboldshimer@Dustins-MacBook-Pro-2.local>
2020-10-29 12:00:29 -0700

    create AppState and provisionally type store as any

--- tests/sitka-test.ts
+++ tests/sitka-test.ts
@@ -1,13 +1,18 @@
-import { ColorModule } from "./color_module"
+import { ColorModule, ColorState } from "./color_module"
 import { Sitka } from  "../src/sitka"
 
 export interface AppModules {
   readonly color: ColorModule
 }
 
+export interface AppState {
+  color: ColorState
+}
+
 const sitka = new Sitka<AppModules>()
 sitka.register([ new ColorModule()])
 
-const store = sitka.createStore()
+// TODO: type correctly.  Might require changes to sitka Create Store
+const store: any = sitka.createStore()
 
 export { sitka, store }

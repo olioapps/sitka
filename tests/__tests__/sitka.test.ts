9bba582f08729b044ea3f8354e93c27bfc42d92e
Dustin Herboldshimer <dustn.pdx@gmail.com>
2021-01-05 09:48:23 -0800

    add describe setDispatch.  rename getModules test

--- tests/__tests__/sitka.test.ts
+++ tests/__tests__/sitka.test.ts
@@ -1,45 +1,48 @@
-import { Action, Dispatch } from "redux"
+import { Dispatch } from "redux"
 import { AppState, store, sitka } from "../sitka-test"
 const { text: textModule } = sitka.getModules()
 
 describe("Sitka", () => {
-    // SETUP
-
-    let foo
-
-    beforeEach(() => {
-      foo = undefined
-      const modules = sitka.getModules()
-      Object.values(modules).forEach((module: any) => {
-        module.handleReset()
-      })
-
-
+  // SETUP
+  beforeEach(() => {
+    const modules = sitka.getModules()
+    Object.values(modules).forEach((module: any) => {
+      module.handleReset()
     })
 
-  test(`setDispatch dispatch was called`, async () => {
-    const mockDispatch: Dispatch = <T>(action: T) => {
-      foo = "bar"
-      store.dispatch(action)
-      return action
-    }
-
-    sitka.setDispatch(mockDispatch)
-    textModule.handleUpdateSize(20)
-
-    const { text: actual } = await store.getState()
-    const expected = {
-      size: 20,
-      value: "Hello World",
-      numberOfEdits: 0,
-      history: ["MODULE_TEXT_HANDLEUPDATESIZE"]
-    }
-    expect(actual).toEqual(expected)
-    expect(foo).toEqual('bar')
   })
 
-  // test(`getModules returns Redux store`, () => {
-  // })
+  describe('setDispatch', () => {
+    // Setup
+    let wasMockDispatched: boolean
+    beforeEach(() => {
+      wasMockDispatched = false
+    })
+
+    test(`setDispatch dispatch was called`, () => {
+      const mockDispatch: Dispatch = <T>(action: T) => {
+        wasMockDispatched = true
+        store.dispatch(action)
+        return action
+      }
+
+      sitka.setDispatch(mockDispatch)
+      textModule.handleUpdateSize(20)
+
+      const { text: actual } = store.getState()
+      const expected = {
+        size: 20,
+        value: "Hello World",
+        numberOfEdits: 0,
+        history: ["MODULE_TEXT_HANDLEUPDATESIZE"]
+      }
+      expect(actual).toEqual(expected)
+      expect(wasMockDispatched).toEqual(true)
+    })
+  })
+
+  test(`getModules returns registered modules`, () => {
+  })
 
   // test(`createSitkaMeta returns expected SitkaMeta`, () => {
   // })
